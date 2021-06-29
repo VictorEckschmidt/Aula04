5d856131a882d046f6f55c5ab82fd4581866924e
Victor Eckschmidt <Victor.eckschmidt@outlook.com.br>
2021-06-28 21:12:01 -0300

    implementado o loop while

---
+++ src/entendendoWhile.java
@@ -0,0 +1,14 @@
+
+public class entendendoWhile {
+
+	public static void main(String[] args) {
+		int a = 0;
+		while(a < 100) {
+			System.out.println("oi!" + a);
+			a = a + 1;
+			
+		}
+		
+	}
+
+}
